13d572daf97e9361e52a3f237e806312aa403822
ddan25 <peterortiz97@yahoo.com>
2024-11-15 09:34:16 -0800

    server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,10 @@
 import express from 'express';
-import path from 'node:path';
 import type { Request, Response } from 'express';
 import db from './config/connection.js'
 import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './services/auth.js';
-import { fileURLToPath } from 'node:url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const server = new ApolloServer({
   typeDefs,
@@ -33,10 +28,10 @@ const startApolloServer = async () => {
   ));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static( '../client/dist'));
 
     app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile('../client/dist/index.html');
     });
   }
 
